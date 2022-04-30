0d0193e2adf07c41b82b381d55a37a24d05b9097
aelgmal7 <aelgmal7@gmail.com>
2022-04-30 07:08:47 +0200

    organise back-end folders && prepare response schema && try some methods

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,14 @@
-const url = require("url");
-const path = require("path");
+// const url = require("url");
+// const path = require("path");
 const express = require("express");
 const cors = require('cors')
 const bodyParser =require('body-parser')
 const temp = require('./temp.ts')
+import {returnedResult} from './Payload/ReturnedResult.js'
+import {HTTP_STATUS_CODES} from './Payload/statusCode.ts' ;
+
 temp.sayHi()
 const server = () => {
-  
 const exp = express();
 exp.use(express.json());
 exp.use(bodyParser.urlencoded({extended:false}))
@@ -30,9 +32,13 @@ exp.get('/tasks',(req,res,next) => {
 })
 exp.post('/tasks',(req,res,next) => {
   const r= req.body.title
+  console.log(returnedResult('CODE_200',true,{title:tasks}))
+  if(r === undefined){
+    throw new Error('no data sent')
+  }
   console.log(r)
   tasks.push(r)
-  res.send(`{"title":"${tasks}"}`)
+  res.send(returnedResult( HTTP_STATUS_CODES['CODE_200'],true,{title:tasks}))
 })
 exp.listen(500)
 
