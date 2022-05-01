b1a5351274ab9487eaee5d3fb8ac7fa2ffda67b8
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-05-01 18:19:48 +0200

    splitted app into two directories and fixed package scripts

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const { app, BrowserWindow, ipcMain, IpcMessageEvent } = require("electron");
-const url = require("url");
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const { app, BrowserWindow, ipcMain, IpcMessageEvent } = require("electron")
+const url = require("url")
+const path = require("path")
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
 
-const { server } = require("./backend");
-server();
+const { server } = require("./backend")
+server()
 
-let appWindow;
+let appWindow
 function initWindow() {
   appWindow = new BrowserWindow({
     width: 1000,
@@ -17,40 +17,40 @@ function initWindow() {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  });
+  })
   // Electron Build Path
   appWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, `/dist/index.html`),
+      pathname: path.join(__dirname, `frontend/dist/index.html`),
       protocol: "file:",
       slashes: true,
     })
-  );
+  )
   // Initialize the DevTools.
-  appWindow.webContents.openDevTools();
+  appWindow.webContents.openDevTools()
   appWindow.on("closed", function () {
-    appWindow = null;
-  });
+    appWindow = null
+  })
 }
 
-app.on("ready", initWindow);
+app.on("ready", initWindow)
 
 // Close when all windows are closed.
 app.on("window-all-closed", function () {
   // On macOS specific close process
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on("activate", function () {
   if (win === null) {
-    initWindow();
+    initWindow()
   }
-});
+})
 
 ipcMain.on("db:add-task", (event, newTask) => {
-  console.log("db:add-task", newTask);
-  tasks.push(newTask);
-  event.sender.send("db:task-added", tasks);
-});
+  console.log("db:add-task", newTask)
+  tasks.push(newTask)
+  event.sender.send("db:task-added", tasks)
+})
