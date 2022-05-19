d9429f5d62897c11cd8002ff100b510c42b7d6d8
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-05-20 01:45:46 +0200

    finished nav mvp

--- frontend/src/app/shared/components/vertical-navbar/vertical-navbar.component.ts
+++ frontend/src/app/shared/components/vertical-navbar/vertical-navbar.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vertical-navbar',
   templateUrl: './vertical-navbar.component.html',
-  styleUrls: ['./vertical-navbar.component.css']
+  styleUrls: ['./vertical-navbar.component.css'],
 })
 export class VerticalNavbarComponent implements OnInit {
+  isNavItemOpen = { accounting: false, store: false };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
