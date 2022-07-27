f87a991a99e33cefae7e68ecbdd76df094b7fdbd
aelgmal7 <aelgmal7@gmail.com>
2022-07-27 17:18:39 +0200

    add pay.ejs and print pay

--- backend/Services/BillService.js
+++ backend/Services/BillService.js
@@ -124,14 +124,98 @@ const payForBill = async(billId,clientId,date, money,note=null) =>{
                 pay.save()
                 return pay
                   
+            }).then(pay => {
+                printPay(client,pay)
+                return pay
             })
         })
     })
 } 
 
+const printPay = async(client,bill) => {
+
+    const pay = await  ejs.renderFile(`${path.join(__dirname,'..',"views","pay.ejs")}`,{bill:bill,client})
+
+    let options = { format: 'A4' };
+    // `${bill.id} ${client.clientName} ${(new Date(bill.date)).toLocaleDateString('en-US')} .pdf`
+    let file = { content: pay };
+    html_to_pdf.generatePdf(file, options).then(async(pdfBuffer) => {
+        const pdfPath =`${client.clientName}/${bill.id}-${client.clientName}-${(new Date(bill.date)).toLocaleDateString("nl",{year:"2-digit",month:"2-digit", day:"2-digit"})}.pdf`
+        console.log("PDF Buffer:-", pdfBuffer);
+        if(process.env.PROD == "true"){
+            const fwaterDirProd = `${path.join(app.getPath('userData'),"مدفوعات")}`
+            const clientDirProd = `${path.join(app.getPath('userData'),"مدفوعات",client.clientName)}`
+
+            try {
+                // first check if directory already exists
+                if (!fs.existsSync(fwaterDirProd)) {
+                    fs.mkdirSync(fwaterDirProd);
+                    console.log("Directory is created.");
+                } else {
+                    console.log("Directory already exists.");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+
+            try {
+                // first check if directory already exists
+                if (!fs.existsSync(clientDirProd)) {
+                    fs.mkdirSync(clientDirProd);
+                    console.log("Directory is created.");
+                } else {
+                    console.log("Directory already exists.");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+                fs.writeFile(`${path.join(path.join(app.getPath('userData'),"مدفوعات"),pdfPath)}`,pdfBuffer,err => {
+                    if(err) {
+                        console.log(err)
+                        // er = err
+                        return err
+                    }
+
+                    require('child_process').exec(`explorer.exe "${path.join(path.join(app.getPath('userData'),"فواتير"),pdfPath)}"`);
+                });
+        }else {
+            const dir = `${path.join("backend","views","مدفوعات",client.clientName)}`
+            const fwater = `${path.join("backend","views","مدفوعات")}`
+            try {
+                // first check if directory already exists
+                if (!fs.existsSync(fwater)) {
+                    fs.mkdirSync(fwater);
+                    console.log("Directory is created.");
+                } else {
+                    console.log("Directory already exists.");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+            try {
+                // first check if directory already exists
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir);
+                    console.log("Directory is created.");
+                } else {
+                    console.log("Directory already exists.");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+            fs.writeFile(`${path.join("backend","views","مدفوعات",pdfPath)}`,pdfBuffer,err => {
+                require('child_process').exec(`explorer.exe "${path.join("backend","views","مدفوعات",pdfPath)}"`);
+                
+            });
+
+        }
+
+    })
+}
 
 
-let coreFn = async (temp,name,client,bill,option) => {
+
+const coreFn = async (temp,name,client,bill,option) => {
 
     let options = { format: 'A4' };
     // `${bill.id} ${client.clientName} ${(new Date(bill.date)).toLocaleDateString('en-US')} .pdf`
